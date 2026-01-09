4842a980b4a6165d1abfa5e40a4497a09cdafe84
Porgetit <kevin.esguerra@utp.edu.co>
2026-01-09 16:04:14 -0500

    Allow Koyeb preview host

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => {
       preview: {
         host: '0.0.0.0',
         port: Number(process.env.PORT) || 3000,
-        allowedHosts: 'all'
+        allowedHosts: ['melodic-halimeda-athirtro-acf98886.koyeb.app']
       },
       plugins: [react()],
       define: {
